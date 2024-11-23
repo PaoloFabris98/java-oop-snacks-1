eda00cb007069e240d6ea924d348e1d87467c124
PaoloFabris98 <f98paolo@gmail.com>
2024-11-23 21:07:28 +0100

    feat: bank Account

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Studente p = new Studente("ciao", "cazzone", 101);
         System.out.println(p.nameReturning());
+
+        bankAccount a = new bankAccount();
+        a.setBalance(500.00);
+        System.out.println(a.returnBalance());
+        a.setBalance(-1500.00);
+        System.out.println(a.returnBalance());
+
     }
 }
--- src/org/classes/bankAccount.java
+++ src/org/classes/bankAccount.java
@@ -1,10 +1,31 @@
 package org.classes;
 
+import java.util.Random;
+
 public class bankAccount {
     private int accountNumber;
     private Double balance;
 
-    public void Studente() {
-        this.balance = 0.0;
+    public bankAccount() {
+        Random random = new Random();
+        this.accountNumber = random.nextInt(1000000);
+        this.balance = 0.00;
     }
+
+    public int returnAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public Double returnBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(Double num) {
+        if (this.balance + num >= 0) {
+            this.balance += num;
+        } else {
+            System.out.println("Non posso compiere l'azione, il conto andrebbe in negativo");
+        }
+    }
+
 }
