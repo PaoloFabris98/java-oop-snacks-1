56572e9dc7e275f6afb0caa64ad7f2f3bd76f4d0
PaoloFabris98 <f98paolo@gmail.com>
2024-11-23 22:55:41 +0100

    feat: student register corrected

--- src/org/classes/StudentRegister.java
+++ src/org/classes/StudentRegister.java
@@ -9,8 +9,12 @@ public class StudentRegister {
     };
 
     public void addStudent(Studente temp) {
-        this.studentList.add(temp);
-    };
+        if (temp.nameReturning() != null && temp.surnameReturning() != null && temp.ageReturning() > 0) {
+            this.studentList.add(temp);
+        } else {
+            System.out.println("Studente non valido, non aggiunto al registro.");
+        }
+    }
 
     public ArrayList<Studente> getStudents() {
         return studentList;
